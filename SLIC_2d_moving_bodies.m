98f632283dd63bbc80f1f2c143bf03f223fddd13
MrBAYoung <ben135young@gmail.com>
2017-03-13 19:16:46 +0000

    Add files via upload
    Implemented SLIC with TVD over the FORCE scheme, tests pending

---
+++ SLIC_2d_moving_bodies.m
@@ -0,0 +1,281 @@
+clear all
+close all
+Lx = 1;
+Ly = 1;
+dx = 0.01;
+[xx,yy] = meshgrid([0:dx:Lx],[0:dx:Ly]);
+U = zeros([size(xx) 4]);
+gamm = 1.4;
+t = 0;
+CFL = 0.9;
+N = size(xx,2);
+M = size(xx,1);
+w = 0;
+Us = [0 0];
+b=1;
+%       |  rho  |
+%U =    | rho u |
+%       | rho v |
+%       |   E   |
+
+
+%Test 1
+
+rho = ones(size(xx));
+p = rho*1.5698;
+v = rho*0.394;
+u = rho*0;
+rho = rho*1.3764;
+
+% 
+rho(21:end,:) = 1;
+u(21:end,:) = 0;
+v(21:end,:) = 0;
+p(21:end,:) = 1;
+
+
+% rho(:,:) = 1;
+% u(:,:) = 0;
+% v(:,:) = 0;
+% p(:,:) = 1;
+% rho(:,21:end) = 1;
+% u(:,21:end) = 0;
+% v(:,21:end) = 0;
+% p(:,21:end) = 1;
+
+% rho(1:end-20,:) = 1;
+% u(1:end-20,:) = 0;
+% v(1:end-20,:) = 0;
+% p(1:end-20,:) = 1;
+
+U(:,:,1) = rho;
+U(:,:,2) = u.*rho;
+U(:,:,3) = v.*rho;
+U(:,:,4) = 0.5*rho.*(u.^2+v.^2) + p/(gamm-1);
+t = 0;
+%phi = sign(abs(real(sqrt(0.05^2 - ((xx-0.3 - Us(1)*t).^2 + (yy-0.5 -Us(2)*t).^2))))-0.001);
+
+phi = 0*xx-1;
+phi(40:60,:) = 1;
+
+phi_s = phi;
+
+for i = 1:10
+    phi_s(2:end-1,2:end-1) = phi_s(2:end-1,2:end-1) + 0.1*(phi_s(2:end-1,1:end-2) + phi_s(2:end-1,3:end) + phi_s(1:end-2,2:end-1) + phi_s(3:end,2:end-1) -4*phi(2:end-1,2:end-1));
+end
+% 
+% 
+% 
+phix = phi*0;
+phiy = phi*0;
+phix(2:end-1,2:end-1) = (phi_s(2:end-1,3:end)-phi_s(2:end-1,1:end-2))/(2*dx);
+phiy(2:end-1,2:end-1) = (phi_s(3:end,2:end-1)-phi_s(1:end-2,2:end-1))/(2*dx);
+%Producing a smoothed phi_z for normal calculations:
+nx = phix./sqrt(phix.^2 + phiy.^2);
+ny = phiy./sqrt(phix.^2 + phiy.^2);
+nx(isnan(nx)==1)=0;
+ny(isnan(ny)==1)=0;
+
+
+
+while t<0.3
+    [U,dt] = SLICstep(U,dx,CFL,w,M,N);
+    %boundary conditions:
+    U(:,end,:) = U(:,end-1,:);
+    U(end,:,:) = U(end-1,:,:);
+    U(1,:,:) = U(2,:,:);
+    U(:,1,:) = U(:,2,:);
+
+    
+    t = t+dt
+    %axis([0 1 0 1 0 1]) 
+    [ rho,u,v,p,e ] = primative2d( U );
+    
+    %creating extrapolated values:
+    rho_ext = rho;
+    u_ext = u;
+    v_ext = v;
+    p_ext = p;
+    dtau = dt/10;
+    
+%     phi = sign(abs(real(sqrt(0.05^2 - ((xx-0.4 - Us(1)*t).^2 + (yy-0.5 -Us(2)*t).^2))))-0.001);
+%     
+%     phi_s = phi;
+%     
+%     for i = 1:10
+%         phi_s(2:end-1,2:end-1) = phi_s(2:end-1,2:end-1) + 0.1*(phi_s(2:end-1,1:end-2) + phi_s(2:end-1,3:end) + phi_s(1:end-2,2:end-1) + phi_s(3:end,2:end-1) -4*phi(2:end-1,2:end-1));
+%     end
+%     
+%     
+%     
+%     phix = phi*0;
+%     phiy = phi*0;
+%     phix(2:end-1,2:end-1) = (phi_s(2:end-1,3:end)-phi_s(2:end-1,1:end-2))/(2*dx);
+%     phiy(2:end-1,2:end-1) = (phi_s(3:end,2:end-1)-phi_s(1:end-2,2:end-1))/(2*dx);
+%     %Producing a smoothed phi_z for normal calculations:
+%     nx = phix./sqrt(phix.^2 + phiy.^2);
+%     ny = phiy./sqrt(phix.^2 + phiy.^2);
+%     nx(isnan(nx)==1)=0;
+%     ny(isnan(ny)==1)=0;
+    
+    
+    for tau = 1:100
+        for i = 2:N-1
+            for j = 2:M-1
+                if phi_s(j,i)>=0 %ie we are in the ghost fluid region
+                    if nx(j,i)>0 %see appendix A.4 in Fedkiw et al.
+                        %calculating upwinded derivatives
+                        drhodx = (rho(j,i) - rho(j,i-1))/dx;
+                        dpdx = (p(j,i) - p(j,i-1))/dx;
+                        %If the velocity derivative uses a value from
+                        %outside of the ghost cell region, the tangential
+                        %component of velocity is interpolated normally,
+                        %but the normal component of velocity must be
+                        %reflected before being interpolated
+                        
+                        if phi_s(j,i-1)>=0 %ie both cells are ghost cells
+                            dudx = (u(j,i) - u(j,i-1))/dx;
+                            dvdx = (v(j,i) - v(j,i-1))/dx;
+                        else
+                            %Initially finding the reflected tangential component of
+                            %velocity of u(j,i-1):
+                            urel = u(j,i-1) - Us(1);
+                            vrel = v(j,i-1) - Us(2);
+                            uref = b*(urel - 2*(urel*nx(j,i) + vrel*ny(j,i))*nx(j,i));
+                            vref = b*(vrel - 2*(urel*nx(j,i) + vrel*ny(j,i))*ny(j,i));
+                            dudx = (u(j,i) - uref)/dx;
+                            dvdx = (v(j,i) - vref)/dx;
+                        end
+                        
+                        
+                    elseif nx(j,i)<0
+                        drhodx = (rho(j,i+1) - rho(j,i))/dx;
+                        dpdx = (p(j,i+1) - p(j,i))/dx;
+                        if phi_s(j,i+1)>=0 %ie both cells are ghost cells
+                            dudx = (u(j,i+1) - u(j,i))/dx;
+                            dvdx = (v(j,i+1) - v(j,i))/dx;
+                        else
+                            %Initially finding the reflected tangential component of
+                            %velocity of u(j,i-1):
+                            urel = u(j,i+1) - Us(1);
+                            vrel = v(j,i+1) - Us(2);
+                            uref = b*(urel - 2*(urel*nx(j,i) + vrel*ny(j,i))*nx(j,i));
+                            vref = b*(vrel - 2*(urel*nx(j,i) + vrel*ny(j,i))*ny(j,i));
+                            dudx = (uref - u(j,i))/dx;
+                            dvdx = (vref - v(j,i))/dx;
+                        end
+                    else
+                        dpdx = 0;
+                        dudx = 0;
+                        drhodx = 0;
+                        dvdx = 0;
+                    end
+                    
+                    if ny(j,i)>0
+                        drhody = (rho(j,i) -rho(j-1,i))/dx;
+                        dpdy = (p(j,i)-p(j-1,i))/dx;
+                        if phi_s(j-1,i)>=0
+                            dudy = (u(j,i)-u(j-1,i))/dx;
+                            dvdy = (v(j,i)-v(j-1,i))/dx;
+                        else
+                            urel = u(j-1,i) - Us(1);
+                            vrel = v(j-1,i) - Us(2);
+                            uref = b*(urel - 2*(urel*nx(j,i) + vrel*ny(j,i))*nx(j,i));
+                            vref = b*(vrel - 2*(urel*nx(j,i) + vrel*ny(j,i))*ny(j,i));
+                            dudy = (u(j,i)-uref)/dx;
+                            dvdy = (v(j,i)-vref)/dx;
+
+                        end
+                    elseif ny(j,i)<0
+                        drhody = (rho(j+1,i)-rho(j,i))/dx;
+                        dpdy = (p(j+1,i)-p(j,i))/dx;
+                        if phi_s(j+1,i)>=0
+                            dudy = (u(j+1,i)-u(j,i))/dx;
+                            dvdy = (v(j+1,i)-v(j,i))/dx;
+                        else
+                            urel = u(j+1,i) - Us(1);
+                            vrel = v(j+1,i) - Us(2);
+                            uref = b*(urel - 2*(urel*nx(j,i) + vrel*ny(j,i))*nx(j,i));
+                            vref = b*(vrel - 2*(urel*nx(j,i) + vrel*ny(j,i))*ny(j,i));
+                            dudy = (uref - u(j,i))/dx;
+                            dvdy = (vref - v(j,i))/dx;
+                        end
+                    else 
+                        dudy = 0;
+                        dvdy = 0;
+                        dpdy = 0;
+                        drhody = 0;
+                    end
+                    
+                    u_ext(j,i) = u(j,i) - dtau*(dudy.*ny(j,i) + dudx.*nx(j,i));
+                    v_ext(j,i) = v(j,i) - dtau*(dvdy.*ny(j,i) + dvdx.*nx(j,i));
+                    p_ext(j,i) = p(j,i) - dtau*(dpdy.*ny(j,i) + dpdx.*nx(j,i));
+                    rho_ext(j,i) = rho(j,i) - dtau*(drhody*ny(j,i) + drhodx*nx(j,i)); 
+                end
+            end
+        end
+
+        u = u_ext;
+        v = v_ext;
+        p = p_ext;
+%         rho = rho_ext;
+%         clf
+%         hold on
+%         surf(p)
+%         surf(phi)
+%         axis([0 100 0 100 0 1])
+%         view(-37.5,60);
+%         pause(0.1)
+        
+        
+    end
+    U(:,:,1) = rho;
+    U(:,:,2) = u.*rho;
+    U(:,:,3) = v.*rho;
+    U(:,:,4) = 0.5*rho.*(u.^2+v.^2) + p/(gamm-1);
+    
+                       
+                    
+                    
+                    
+                    
+                   
+%         u = u_ext;
+%         v = v_ext;
+%         p = p_ext;
+%         rho = rho_ext;
+%         clf
+%         hold on
+%         %surf(p)
+%         %plot(p(50,:))
+%         %surf(phi*2.6)
+%        axis([0 100 0 100 0 2.5])
+%         view(-37.5,60);
+%         pause(0.1)
+%surf(p.*(sign(-phi)+1)/2)
+
+%imagesc(p.*(sign(-phi)+1)/2);
+%mock schlieren:
+
+grad_rho = abs((rho(3:end,2:end-1) - rho(1:end-2,2:end-1)))/(2*dx) + abs(rho(2:end-1,3:end)-rho(2:end-1,1:end-2))/(2*dx);
+schil = exp(-20*abs(grad_rho)./(1000*rho(2:end-1,2:end-1)));
+schil = schil.*(sign(-phi(2:end-1,2:end-1))+1)/2;
+schil(schil==0)=1;
+%surf(schil);
+%image(50*schil);
+%imagesc(schil.*(sign(-phi(2:end-1,2:end-1))+1)/2);
+%imagesc(schil);
+% plot(rho(:,37));
+% hold on
+% plot(rho(:,65));
+% %plot(rho(:,32),'x');
+% hold off
+%surf(p);
+%quiver(u,v);
+%view(60,60);
+schil_pad = ones(M,N);
+%imagesc(-log(schil));
+plot(p(:,50));
+schil_pad(2:end-1,2:end-1) = schil;
+pause(0.0001);
+end
---
+++ SLICstep.m
@@ -0,0 +1,149 @@
+function [ U,dt ] = SLICstep( U,dx,CFL,w,M,N)
+%UNTITLED7 Summary of this function goes here
+%   Detailed explanation goes here
+gamm = 1.4;
+Uinit = U;
+
+%Using parallel splitting, ie: e^(A + B) \approx 0.5*(e^(A)e^(B) +
+%e^(B)e^(A).
+
+    [ rho,u,v,p,e ] = primative2d( U );
+    a = sqrt(gamm*p./rho);
+    cmax = max(max(a + sqrt(u.^2 + v.^2)));
+    dt = (CFL*dx)/cmax;
+    
+    
+    %Solving in the x direction initially
+    
+    %creating a TVD flux limiter (see Toro for details)
+    delta_i2 = U(:,2:end,:)-U(:,1:end-1,:);
+    r = delta_i2(:,1:end-1,:)./delta_i2(:,2:end,:);
+    r(isnan(r)==1)=0; %removing nan values from 0/0;
+    zetasb = zeros(M,N-2); %Creating superbee flux vector
+    Beta = 1; %Limiter parameter
+    ZetaR = 2*Beta./(1-w +(1+w).*r); %Rightward upwinded flux limiter
+    
+    %looping over all values of r to create the flux limitng variable:
+    k = 1; 
+    %k determines which of the primitive variables will be 
+    %used for flux limiting, default is k = 1, ie oscillations in the
+    %density will be used for the flux limiter calculations
+    
+    for i = 1:N-2
+        for j = 2:M-1
+            if r(j,i,k)<=0
+                zetasb(j,i) = 0;
+            elseif r(j,i,k) <=0.5
+                zetasb(j,i) = 2*r(j,i,k);
+            elseif r(j,i,k) <= 1;
+                zetasb(j,i) = 1;
+            else
+                zetasb(j,i) = min([r(j,i,k) ZetaR(j,i,k) 2]);
+            end
+        end
+    end
+    
+   %Applying flux limiter to interpolated values:
+   for i = 1:4
+       delta_i(:,1:N-2,i) = zetasb(:,1:N-2).*(0.5*(1+w).*delta_i2(:,1:N-2,i) + 0.5*(1-w).*delta_i2(:,2:N-1,i));
+   end
+    
+   %Stepping through half a time step using HLLC fluxes
+    ULs = U(:,2:end-1,:) - 0.5*delta_i;
+    URs = U(:,2:end-1,:) + 0.5*delta_i;
+    
+    ULbar = ULs + 0.5*(dt/dx)*(flux_calc2d(ULs,1) - flux_calc2d(URs,1));
+    URbar = URs + 0.5*(dt/dx)*(flux_calc2d(ULs,1) - flux_calc2d(URs,1));
+    
+    UL = URbar(:,1:end-1,:);
+    UR = ULbar(:,2:end,:);
+    
+    %Using force to step through the rest of the time step.
+    ULW = 0.5*(UL+UR) - 0.5*(dt/dx)*(flux_calc2d(UR,1)-flux_calc2d(UL,1));
+    FLW = flux_calc2d(ULW,1);
+    
+    
+    Fforce = 0.25*(flux_calc2d(UL,1) + 2*FLW + flux_calc2d(UR,1) - (dx/dt)*(UR - UL));
+    Unew = U;
+    
+    Unew(:,3:end-2,:) = U(:,3:end-2,:) -(dt/dx)*(Fforce(:,2:end,:)- Fforce(:,1:end-1,:));
+    U = Unew;
+    
+    U(:,end,:) = U(:,end-2,:);
+    U(:,end-1,:) = U(:,end-2,:);
+    U(end,:,:) = U(end-2,:,:);
+    U(end-1,:,:) = U(end-2,:,:);
+    U(1,:,:) = U(3,:,:);
+    U(2,:,:) = U(3,:,:);
+    U(:,1,:) = U(:,3,:);
+    U(:,2,:) = U(:,3,:);
+    
+    %Solving in the y direction
+
+    delta_i2 = U(2:end,:,:) - U(1:end-1,:,:);
+    r = delta_i2(1:end-1,:,:)./delta_i2(2:end,:,:);
+    r(isnan(r)==1)=0;
+    zetasb = zeros(M-2,N);
+    zetaR = 2*Beta./(1-w + (1+w).*r);
+    delta_i = [];
+    %delta_i = zeros(
+    
+    %again using density for TVD limiter:
+    k = 1;
+    for i = 2:N-1
+        for j = 1:M-2
+            if r(j,i,k)<=0
+                zetasb(j,i) = 0;
+            elseif r(j,i,k)<=1
+                zetasb(j,i) = 2*r(j,i,k);
+            elseif r(j,i,k)<=2
+                zetasb(j,i) = 1;
+            else
+                zetasb(j,i,k) = min([r(j,i,k) zetaR(j,i,k) 2]);
+            end
+        end
+    end
+
+    for i = 1:4
+        delta_i(:,:,i) = zetasb(1:M-2,:).*(0.5*(1+w).*delta_i2(1:M-2,:,i) + 0.5*(1-w).*delta_i2(2:M-1,:,i));
+    end
+    
+    ULs = U(2:end-1,:,:) - 0.5*delta_i;
+    URs = U(2:end-1,:,:) + 0.5*delta_i;
+    
+    ULbar = ULs +0.5*(dt/dx)*(flux_calc2d(ULs,2) - flux_calc2d(URs,2));
+    URbar = URs + 0.5*(dt/dx)*(flux_calc2d(ULs,2) - flux_calc2d(URs,2));
+    
+    UL = URbar(1:end-1,:,:);
+    UR = ULbar(2:end,:,:);
+    
+    
+    ULW = 0.5*(UL+UR) - 0.5*(dt/dx)*(flux_calc2d(UR,2)-flux_calc2d(UL,2));
+    FLW = flux_calc2d(ULW,2);
+    
+    
+    Fforce = 0.25*(flux_calc2d(UL,2) + 2*FLW + flux_calc2d(UR,2) - (dx/dt)*(UR - UL));
+    Unew = U;
+    
+    Unew(3:end-2,:,:) = U(3:end-2,:,:) -(dt/dx)*(Fforce(2:end,:,:)- Fforce(1:end-1,:,:));
+    
+    
+    U = Unew;
+    
+    U(:,end,:) = U(:,end-2,:);
+    U(:,end-1,:) = U(:,end-2,:);
+    U(end,:,:) = U(end-2,:,:);
+    U(end-1,:,:) = U(end-2,:,:);
+    U(1,:,:) = U(3,:,:);
+    U(2,:,:) = U(3,:,:);
+    U(:,1,:) = U(:,3,:);
+    U(:,2,:) = U(:,3,:);
+    
+    
+     UHS = 0.5*U;
+%     
+ 
+
+    
+end
+
